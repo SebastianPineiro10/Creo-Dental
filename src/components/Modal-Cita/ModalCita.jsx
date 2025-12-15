6853c92b200962f502a552ed3b5f0e87902c4eff
SebastianPineiro10 <sebastian.bienesraices10@gmail.com>
2025-12-15 10:12:43 -0700

    Nuevos cambios

--- src/components/Modal-Cita/ModalCita.jsx
+++ src/components/Modal-Cita/ModalCita.jsx
@@ -8,7 +8,7 @@ export default function ModalCita({ open, onClose }) {
 
   // URL DE TU APPS SCRIPT
   const APPS_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbz2a16YVpcmBnA4g41x4bjaV-4Wy7hiDJQi0EH37ZuPtG_KkYb4CTXd8_6Dzi9ziY-B/exec";
+    "https://script.google.com/macros/s/AKfycbwr8AsR0TMHlQ0V2tkT6jdfjOUFjV6l3N49NjruqNQxp-wXbJDZogsDm9ipEcr0RTuZ/exec";
 
   if (!open) return null;
 
